0ab6ee849ed0f7a9fe49dbc3c2d706fbb7e4d970
William Long <willhlong@gmail.com>
2023-05-27 15:01:14 -0400

    Added GameBoard, Player, GameController

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const Player = (number, symbol) => {
+    
+    const getNumber = () => number;
+    const getSymbol = () => symbol;
+
+    return {getNumber, getSymbol};
+};
+
 const GameBoard = (() => {
     let boardArray = 
     [ 
@@ -6,7 +14,7 @@ const GameBoard = (() => {
         ' ', ' ', ' '
     ];
     const draw = () => {
-        //FIXME: add draw to screen functionality
+        console.log(boardArray);
     }
     const updateBoard = (position, symbol) => {
         boardArray[position - 1] = symbol;
@@ -15,16 +23,17 @@ const GameBoard = (() => {
         //FIXME: clear board array
     }
     const winner = () => {
-        if      (boardArray[0] === boardArray[1] === boardArray[2]) {return true;}
-        else if (boardArray[0] === boardArray[3] === boardArray[6]) {return true;}
-        else if (boardArray[0] === boardArray[4] === boardArray[8]) {return true;}
-        else if (boardArray[1] === boardArray[4] === boardArray[7]) {return true;}
-        else if (boardArray[2] === boardArray[5] === boardArray[8]) {return true;}
-        else if (boardArray[3] === boardArray[4] === boardArray[5]) {return true;}
-        else if (boardArray[6] === boardArray[7] === boardArray[8]) {return true;}
-        else if (boardArray[2] === boardArray[4] === boardArray[6]) {return true;}
+        if      (boardArray[0] === boardArray[1] && boardArray[1] === boardArray[2] && boardArray[2] !== ' ') {return true;}
+        else if (boardArray[0] === boardArray[3] && boardArray[3] === boardArray[6] && boardArray[6] !== ' ') {return true;}
+        else if (boardArray[0] === boardArray[4] && boardArray[4] === boardArray[8] && boardArray[8] !== ' ') {return true;}
+        else if (boardArray[1] === boardArray[4] && boardArray[4] === boardArray[7] && boardArray[7] !== ' ') {return true;}
+        else if (boardArray[2] === boardArray[5] && boardArray[5] === boardArray[8] && boardArray[8] !== ' ') {return true;}
+        else if (boardArray[3] === boardArray[4] && boardArray[4] === boardArray[5] && boardArray[5] !== ' ') {return true;}
+        else if (boardArray[6] === boardArray[7] && boardArray[7] === boardArray[8] && boardArray[8] !== ' ') {return true;}
+        else if (boardArray[2] === boardArray[4] && boardArray[4] === boardArray[6] && boardArray[6] !== ' ') {return true;}
+        else return false;
     }
-    
+    return {draw, updateBoard, reset, winner};
 })();
 
 const GameController = (() => {
@@ -33,11 +42,11 @@ const GameController = (() => {
     const _playerOne = Player(1, 'X');
     const _playerTwo = Player(2, 'O');
     const _getPlayerInput = () => {
-        if (_turnCount % 2 === 1) {
-            return prompt("Player 1, please pick a square...");
+        if (_turnCount % 2 === 0) {
+            return prompt("Player 2, please pick a square...");
         }
         else {
-            return prompt("Player 2, please pick a square...");
+            return prompt("Player 1, please pick a square...");
         }        
     };
     const _turnAdvnace = () => {
@@ -50,7 +59,7 @@ const GameController = (() => {
     const startGame = () => {
         GameBoard.reset();
         GameBoard.draw();
-        _turnCount = 0;
+        _turnCount = 1;
     }
     const winner = () => GameBoard.winner();
     
@@ -58,16 +67,13 @@ const GameController = (() => {
         let currentPlayer;
         _turnCount % 2 === 1 ? currentPlayer = _playerOne : currentPlayer = _playerTwo;
         GameBoard.updateBoard(_getPlayerInput(), currentPlayer.getSymbol());
+        GameBoard.draw();
         _turnAdvnace();
     }
+    return {startGame, winner, takeTurn, getTurnCount};
 })();
 
-const Player = (number, symbol) => {
-    
-    const getNumber = () => number;
-    const getSymbol = () => symbol;
-
-    return {getNumber, getSymbol};
-};
-
 GameController.startGame();
+while (!GameController.winner()) {
+    GameController.takeTurn();
+}
