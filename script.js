5cd86c4ed2b642af26d6cece4b51575152a3f5ad
William Long <willhlong@gmail.com>
2023-05-29 14:07:25 -0400

    Moves correctly draw to game board ui

--- script.js
+++ script.js
@@ -13,11 +13,23 @@ const GameBoard = (() => {
         ' ', ' ', ' ',
         ' ', ' ', ' '
     ];
-    const draw = () => {
-        console.log(boardArray);
+    const draw = (updatedPosition, symbol) => {
+        let i = 1;
+        for (i = 1; i <= boardArray.length; i++) {
+            let gameSquare = document.getElementById("square" + i);
+            if (boardArray[i -1] === 'X') {
+                gameSquare.textContent = 'X';
+            }
+            else if (boardArray[i -1] === 'O') {
+                gameSquare.textContent = 'O';
+            }
+        }
     }
-    const updateBoard = (position, symbol) => {
-        boardArray[position - 1] = symbol;
+    const updateBoard = (position) => {
+        let player = GameController.getCurrentPlayer();
+        boardArray[position] = player.getSymbol();
+        draw();
+        GameController.turnAdvnace();
     }
     const reset = () => {
         //FIXME: clear board array
@@ -41,21 +53,29 @@ const GameController = (() => {
     let _turnCount = 1;
     const _playerOne = Player(1, 'X');
     const _playerTwo = Player(2, 'O');
-    const _getPlayerInput = () => {
-        if (_turnCount % 2 === 0) {
-            return prompt("Player 2, please pick a square...");
-        }
-        else {
-            return prompt("Player 1, please pick a square...");
-        }        
-    };
-    const _turnAdvnace = () => {
+    const turnAdvnace = () => {
         console.log("Next turn (turn number " + _turnCount);
         _incrementTurnCounter();
     }
     const _incrementTurnCounter = () => ++_turnCount;
+    const attachEventListeners = () => {
+        let gameSquares = document.querySelectorAll(".game-square");
+        gameSquares.forEach((gameSquare, index) => {
+            gameSquare.addEventListener("click", () => {
+                GameBoard.updateBoard(index);
+            });
+        });
+    }
     const getTurnCount = () => _turnCount;
-
+    const getCurrentPlayer = () => {
+        let currentPlayer;
+        if (_turnCount % 2 == 1) {
+            return _playerOne;
+        }
+        else {
+            return _playerTwo;
+        }
+    }
     const startGame = () => {
         GameBoard.reset();
         GameBoard.draw();
@@ -63,22 +83,31 @@ const GameController = (() => {
     }
 
     const declareWinner = () => {
-        let currentPlayer;
-        _turnCount % 2 === 1 ? currentPlayer = _playerOne : currentPlayer = _playerTwo;
-        console.log("Player " + currentPlayer.getNumber() + " wins!");
+        if (_turnCount == 9) {
+            console.log("Draw. You both lose. Good day.");
+        }
+            else {
+            let currentPlayer;
+            _turnCount % 2 === 1 ? currentPlayer = _playerOne : currentPlayer = _playerTwo;
+            console.log("Player " + currentPlayer.getNumber() + " wins!");
+        }
     }
     
-    const takeTurn = () => {
-        let currentPlayer;
-        _turnCount % 2 === 1 ? currentPlayer = _playerOne : currentPlayer = _playerTwo;
-        GameBoard.updateBoard(_getPlayerInput(), currentPlayer.getSymbol());
-        GameBoard.draw();
-        _turnAdvnace();
-    }
-    return {startGame, declareWinner, takeTurn, getTurnCount};
+    // const takeTurn = () => {
+    //     GameBoard.updateBoard(_getPlayerInput(), currentPlayer.getSymbol());
+    //     GameBoard.draw();
+    //     _turnAdvnace();
+    // }
+    return {startGame, declareWinner, turnAdvnace, getTurnCount, getCurrentPlayer, attachEventListeners};
 })();
 
-// GameController.startGame();
+
+
+GameController.startGame();
+GameController.attachEventListeners();
+if (GameBoard.winner() || GameController.getTurnCount >= 9) {
+    GameController.declareWinner();
+}
 // while (!GameBoard.winner() && GameController.getTurnCount() <= 9) {
 //     GameController.takeTurn();
 // }
